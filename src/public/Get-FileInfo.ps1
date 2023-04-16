32454ac753044476c66fd9f6c74807f463cddbd0
Calvin Gonzales <105979660+gonzalc@users.noreply.github.com>
2023-04-15 23:10:23 -0500

    Update Get-FileInfo.ps1

--- src/public/Get-FileInfo.ps1
+++ src/public/Get-FileInfo.ps1
@@ -1,6 +1,72 @@
 function Get-FileInfo {
-    <#
-    
-    #>
-    
+<#
+
+#>
+    [CmdletBinding(
+        SupportsShouldProcess    = $true
+        ,DefaultParameterSetName = 'Standard'
+        ,HelpURI                 = 'https://www.google.com'
+    )]
+    param(
+        ###########################################################################################################################
+        #                                              PATH                                                                       #
+        ###########################################################################################################################
+        [Parameter(
+                    Position                         = 1
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Standard'
+                    ,ValueFromPipeline               = $true
+                    ,HelpMessage                     = "This will accept the fullname of files and/or folders to retrieve information."
+        )]
+        [System.Collections.Generic.List[String]]$Path,
+        ###########################################################################################################################
+        #                                              LITERAL_PATH                                                               #
+        ###########################################################################################################################
+        [Parameter(
+                    Position                         = 1
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Literal'
+                    ,ValueFromPipeline               = $true
+                    ,HelpMessage                     = "This will accept the fullname of files and/or folders to retrieve information."
+        )]
+        [System.Collections.Generic.List[String]]$LiteralPath,
+        ###########################################################################################################################
+        #                                              HASH                                                                       #
+        ###########################################################################################################################
+        [Parameter(
+                    Position                         = 2
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Standard'
+                    ,HelpMessage                     = 'This will retrieve hash of each object specified in "-LiteralPath".'
+        )]
+        [Parameter(
+                    Position                         = 2
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Literal'
+                    ,HelpMessage                     = 'This will retrieve hash of each object specified in "-LiteralPath".'
+        )]
+        [ValidateSet('SHA1','SHA256','SHA284','SHA512')]
+        [Alias('Algorithm')]
+        [string]$Hash,
+        ###########################################################################################################################
+        #                                              PASSTHRU                                                                   #
+        ###########################################################################################################################
+        [Parameter(
+                    Position                         = 3
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Standard'
+                    ,HelpMessage                     = 'This will print the results'
+        )]
+        [Parameter(
+                    Position                         = 3
+                    ,Mandatory                       = $false
+                    ,ParameterSetName                = 'Literal'
+                    ,HelpMessage                     = 'This will print the results'
+        )]
+        [switch]$PassThru
+    )
+
+    begin {}
+    process {}
+    end {}
 }
