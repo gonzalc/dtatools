0a85698ba721d4b6d70e234da6d3ea969a98cbaf
Calvin Gonzales <105979660+gonzalc@users.noreply.github.com>
2023-04-20 14:01:44 -0500

    Create Split-Disc.ps1

---
+++ src/public/Split-Disc.ps1
@@ -0,0 +1,3 @@
+function Split-Disc {
+  # This will break up a large folder to fit on specified quantity of discs. You can specify the type of disc that you have available (e.g., 4 CD, 2 DVD, 1 Blu-Ray, etc.).
+}
